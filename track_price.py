5f2d3886ef0d0f76fa5f3b1c31c36bb72f0b0f8e
Axel9206 <91930204+Axel9206@users.noreply.github.com>
2021-12-09 12:19:43 -0600

    Create track_price.py

---
+++ track_price.py
@@ -0,0 +1,18 @@
+def precio_bajo(precio,precio_anterior):
+    if precio < precio_anterior:
+        precio_compra_bajo = (precio * 1.01)
+        return(precio_compra_bajo)
+
+def compra_baja(precio, precio_compra_bajo):
+    if precio <= precio_anterior:
+        print("Compra", precio)
+        return True
+
+precio_anterior,n = 0, 0
+for i in precios:
+    precio_compra_bajo = precio_bajo(i,precio_anterior)
+    #print (precio_compra_bajo)
+    if compra_baja(i, precio_compra_bajo):
+        break
+    precio_anterior = i
+    n+=1
